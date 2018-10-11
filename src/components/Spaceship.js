cfd3d033026829266e14b239a621c51553a45cb7
Suchitra Naik <susanaik@gmail.com>
2018-10-11 14:53:47 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,5 +1,9 @@
 // Code The Spaceship Component Here
 import React from 'react';
+<<<<<<< HEAD
+=======
+import ReactDOM from 'react-dom';
+>>>>>>> 439a2675456aeb6d172d0fc6521a3da9774b0a97
 
 export default class Spaceship extends React.Component {
   render() {
